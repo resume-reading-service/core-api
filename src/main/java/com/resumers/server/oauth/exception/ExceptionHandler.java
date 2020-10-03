e4722aab5526eadf397053eeded1c9022e1a1940
sehajyang <sehajyang@gmail.com>
2020-10-03 20:21:48 +0900

    feat : 디렉터리 생성을 위한 빈 클래스 생성

---
+++ src/main/java/com/resumers/server/oauth/exception/ExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.resumers.server.oauth.exception;
+
+/**
+ * Created by sehajyang
+ * DateTime : 2020/10/03
+ */
+public class ExceptionHandler {
+}
---
+++ src/main/java/com/resumers/server/oauth/filter/LoggingFilter.java
@@ -0,0 +1,9 @@
+package com.resumers.server.oauth.filter;
+
+/**
+ * Created by sehajyang
+ * DateTime : 2020/10/03
+ * Descrpition :
+ */
+public class LoggingFilter {
+}
